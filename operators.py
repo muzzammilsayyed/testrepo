b7a34c4de2035e538d6d7deb55f2328dd4ff3569
muzzammilsayyed <163900396+muzzammilsayyed@users.noreply.github.com>
2024-03-18 23:25:56 +0530

    Add files via upload
    just a simple file on operators in python

---
+++ operators.py
@@ -0,0 +1,28 @@
+#int
+num = 10
+num2 = 20
+print(num + num2)
+print(num - num2)
+print(num * num2)
+print(num / num2)
+print(num % num2)
+#float
+num3 = 5.5
+num4 = 9.6
+print(num3+num4)
+print(num3-num4)
+print(num3*num4)
+print(num3/num4)
+print(num3%num4)
+#complex- can perform if both values complex
+num5 = 3j
+num6 = 5j
+print(num5+num6)
+print(num5-num6)
+print(num5*num6)
+print(num5/num6)
+print(num5%num6)
+#concatenation of string - not addition
+num7 = '54'
+num8 =  90 #'56'
+print(num7+num8)
